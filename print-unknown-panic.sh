d6521470d9b63043c71cf153ecc9703adfd9334f
Frédéric Wang Nélar <fwang@igalia.com>
2026-04-27 13:58:00 +0200

    fixup! update unknown panic messages

--- print-unknown-panic.sh
+++ print-unknown-panic.sh
@@ -36,6 +36,7 @@ for file in $(ls $TESTCASE_DIRECTORY/*.txt); do
     | sed -e "/called \`Result::unwrap()\` on an \`Err\` value: BoolError { message: \"Failed to link elements/,+3d" \
     | sed -e "/called \`Result::unwrap()\` on an \`Err\` value: \"element linking failed: BoolError/,+3d" \
     | sed -e "/internal error: entered unreachable code (.\+src\/picture.rs:[0-9]\+)$/,+3d" \
+    | sed -e "/internal error: entered unreachable code: Found hoisted box with missing fragment. (.\+components\/layout\/display_list\/stacking_context.rs:[0-9]\+)$/,+3d" \
     | grep -B5 -A10 'servoshell::panic_hook::panic_hook' \
     ;
     if [ $? -eq 0 ]; then

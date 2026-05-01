e3823febad68c454bc8b6955e51493def24653cc
Frédéric Wang Nélar <fwang@igalia.com>
2026-05-01 11:16:47 +0200

    update known panic messages

--- print-unknown-panic.sh
+++ print-unknown-panic.sh
@@ -7,37 +7,40 @@ else
     TESTCASE_DIRECTORY=$1
 fi
 
-# The panic messages are selected obtained by grepping around the pattern
-# "servoshell::panic_hook::panic_hook".
+# The panic messages are obtained by grepping around the pattern
+# "servoshell::panic_hook::panic_hook", which is located in the backtrace
+# a few lines below the actual panic messages. Known panic messages (and
+# corresponding "servoshell::panic_hook::panic_hook") are filtered out
+# using the commands like this one:
 #
-# Use sed -e '/message/, +3d' to delete already reported issues.
+# sed -e '/your panic message (.\+path\/to\/source\/code.rs:[0-9]\+)$/,+3d' | # Link to GitHub report
 #
-# Notes:
+# Some notes:
 # - Use backslash to escape backtick and other special characters.
-# - If the panic message spans more than one line, use +4d, +5d, etc.
-# - If the panic message is too generic please use a more specific regexp e.g.
-#   "/called \`Option::unwrap()\` on a \`None\` value (.\+prim_store\/image.rs:[0-9]\+)$/,+3d"
+# - If the panic message spans more than one line, use +4d, +5d, etc to
+#   remove the corresponding "servoshell::panic_hook::panic_hook".
+# - Different bugs may generate very similar messages. Please make sure
+#   to make the message as specific as possible (in particular please
+#   include the path to the rust file) so that fuzzers are not blocked
+#   on similar issues to be fixed. Please also include the link to the
+#   GitHub report.
+# - Yes, this bash script is very rudimentary and could be made more
+#   robust, but it does the job  for now...
 
 for file in $(ls $TESTCASE_DIRECTORY/*.txt); do
-    cat $file \
-    | sed -e "/Cache should have been filled from traversal/,+3d" \
-    | sed -e "/PainterSurfmanDetails not found for PainterId (.\+components\/webgl\/webgl_thread.rs:[0-9]\+)/,+3d" \
-    | sed -e "/Parsing shouldn't fail as descriptors are valid by construction: Syntax(None) (.\+components\/script\/dom\/css\/fontface.rs:[0-9]\+)/,+3d" \
-    | sed -e "/Should always have a StackingContextTree for offset parent queries (.\+components\/layout\/layout_impl.rs:[0-9]\+)/,+3d" \
-    | sed -e "/Should only call \`scrollable_overflow()\` after calculating overflow (.\+box_fragment.rs:[0-9]\+)/,+3d" \
-    | sed -e "/assertion failed: count_cell.get() > 0 (.\+components\/script\/dom\/document\/document.rs:[0-9]\+)/,+3d" \
-    | sed -e "/assertion failed: src.width == map.width && src.width == dest.width/,+3d" \
-    | sed -e "/bug: unable to map mix-blend content into parent (.\+src\/picture.rs:[0-9]\+)/,+3d" \
-    | sed -e "/called \`Option::unwrap()\` on a \`None\` value (.\+nodelist.rs:[0-9]\+)$/,+3d" \
-    | sed -e "/called \`Option::unwrap()\` on a \`None\` value (.\+src\/point.rs:[0-9]\+)$/,+3d" \
-    | sed -e "/called \`Option::unwrap()\` on a \`None\` value (.\+src\/rect.rs:[0-9]\+)$/,+3d" \
-    | sed -e "/called \`Option::unwrap()\` on a \`None\` value (.\+style\/matching.rs:[0-9]\+)$/,+3d" \
-    | sed -e "/called \`Result::unwrap()\` on an \`Err\` value: BoolError { message: \"Failed to link elements/,+3d" \
-    | sed -e "/called \`Result::unwrap()\` on an \`Err\` value: \"element linking failed: BoolError/,+3d" \
-    | sed -e "/internal error: entered unreachable code (.\+src\/picture.rs:[0-9]\+)$/,+3d" \
-    | sed -e "/internal error: entered unreachable code: Found hoisted box with missing fragment. (.\+components\/layout\/display_list\/stacking_context.rs:[0-9]\+)$/,+3d" \
-    | grep -B5 -A10 'servoshell::panic_hook::panic_hook' \
-    ;
+    cat $file |
+    sed -e "/Cache should have been filled from traversal (.\+components\/media\/audio\/graph.rs:[0-9]\+)$/,+3d" | # https://github.com/servo/servo/issues/36850
+    sed -e "/PainterSurfmanDetails not found for PainterId (.\+components\/webgl\/webgl_thread.rs:[0-9]\+)$/,+3d" | # https://github.com/servo/servo/issues/42414
+    sed -e "/Parsing shouldn't fail as descriptors are valid by construction: Syntax(None) (.\+components\/script\/dom\/css\/fontface.rs:[0-9]\+)$/,+3d" | # https://github.com/servo/servo/issues/44537
+    sed -e "/Should only call \`scrollable_overflow()\` after calculating overflow (.\+components\/layout\/fragment_tree\/box_fragment.rs:[0-9]\+)$/,+3d" | # https://github.com/servo/servo/issues/41207
+    sed -e "/assertion failed: count_cell.get() > 0 (.\+components\/script\/dom\/document\/document.rs:[0-9]\+)$/,+3d" | # https://github.com/servo/servo/issues/42854
+    sed -e "/assertion failed: src.width == map.width && src.width == dest.width (.\+displacement_map.rs:[0-9]\+)$/,+3d" | # https://github.com/servo/servo/issues/38849
+    sed -e "/bug: unable to map mix-blend content into parent (.\+src\/picture.rs:[0-9]\+)$/,+3d" | # https://github.com/servo/servo/issues/42292
+    sed -e "/called \`Option::unwrap()\` on a \`None\` value (.\+src\/point.rs:[0-9]\+)$/,+3d" | # https://github.com/servo/servo/issues/36870
+    sed -e "/called \`Option::unwrap()\` on a \`None\` value (.\+src\/rect.rs:[0-9]\+)$/,+3d" | # https://github.com/servo/servo/issues/42258
+    sed -e "/internal error: entered unreachable code (.\+src\/picture.rs:[0-9]\+)$/,+3d" | # https://github.com/servo/servo/issues/42476
+    sed -e "/internal error: entered unreachable code: Found hoisted box with missing fragment. (.\+components\/layout\/display_list\/stacking_context.rs:[0-9]\+)$/,+3d" | # https://github.com/servo/servo/issues/44540
+    grep -B5 -A10 'servoshell::panic_hook::panic_hook';
     if [ $? -eq 0 ]; then
         echo -n $file | sed "s/.txt//"
         echo " crashed!"

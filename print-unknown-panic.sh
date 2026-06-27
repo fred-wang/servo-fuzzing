70f3537f55fbcd6b2fde099dcf3fcc4a5ed4de0f
Frédéric Wang Nélar <fwang@igalia.com>
2026-06-27 08:53:13 +0200

    more panic messages

--- print-unknown-panic.sh
+++ print-unknown-panic.sh
@@ -35,6 +35,8 @@ for file in $(ls $TESTCASE_DIRECTORY/*.txt); do
     sed -e "/OriginZero grid line cannot be less than the number of negative grid lines (.\+grid\/types\/coordinates.rs:[0-9]\+)$/,+3d" | # https://github.com/servo/servo/issues/45939
     sed -e "/OriginZero grid line cannot be more than the number of positive grid lines (.\+grid\/types\/coordinates.rs:[0-9]\+)$/,+3d" | # https://github.com/servo/servo/issues/45938
     sed -e "/Parsing shouldn't fail as descriptors are valid by construction: Syntax(None) (.\+components\/script\/dom\/css\/fontface.rs:[0-9]\+)$/,+3d" | # https://github.com/servo/servo/issues/44537
+    sed -e "/Should never try to get clip before adding it to WebRender display list (.\+layout\/display_list\/mod.rs:[0-9]\+)$/,+3d" | # https://github.com/servo/servo/issues/46021
+    sed -e "/animate should only be used for interpolating or accumulating transforms (.\+style\/values\/animated\/transform.rs:[0-9]\+)$/,+3d" | # https://github.com/servo/servo/issues/45981
     sed -e "/assertion failed: \*progress > 1. (.\+style\/servo\/animation.rs:[0-9]\+)$/,+3d" | # https://github.com/servo/servo/issues/45950
     sed -e "/assertion failed: count_cell.get() > 0 (.\+components\/script\/dom\/document\/document.rs:[0-9]\+)$/,+3d" | # https://github.com/servo/servo/issues/42854
     sed -e "/assertion failed: self.inclusive_ancestors(shadow_including).any(|ancestor|$/,+4d" | # https://github.com/servo/servo/issues/45947
@@ -48,7 +50,11 @@ for file in $(ls $TESTCASE_DIRECTORY/*.txt); do
     sed -e "/called \`Option::unwrap()\` on a \`None\` value (.\+src\/rect.rs:[0-9]\+)$/,+3d" | # https://github.com/servo/servo/issues/42258
     sed -e "/called \`Result::unwrap()\` on an \`Err\` value: TryFromIntError(()) (.\+taffy\/stylo_taffy\/convert.rs:[0-9]\+)$/,+3d" | # https://github.com/servo/servo/issues/45881
     sed -e "/entered unreachable code: Unexpected direct descendant PositioningContext of inline. (.\+components\/layout\/display_list\/paint_traversal.rs:[0-9]\+)$/,+3d" | # https://github.com/servo/servo/issues/45740
+    sed -e "/index out of bounds: the len is [0-9]\+ but the index is [0-9]\+ (.\+css\/cssrulelist.rs:[0-9]\+)$/,+3d" | # https://github.com/servo/servo/issues/46025
+    sed -e "/index out of bounds: the len is [0-9]\+ but the index is [0-9]\+ (.\+layout\/display_list\/clip.rs:[0-9]\+)$/,+3d" | # https://github.com/servo/servo/issues/46026
+    sed -e "/index out of bounds: the len is [0-9]\+ but the index is [0-9]\+ (.\+paint\/display_list.rs:[0-9]\+)$/,+3d" | # https://github.com/servo/servo/issues/46023
     sed -e "/internal error: entered unreachable code (.\+src\/tile_cache\/mod.rs:[0-9]\+)$/,+3d" | # https://github.com/servo/servo/issues/45740
+    sed -e "/slice index starts at [0-9]\+ but ends at [0-9]\+ (.\+grid\/types\/grid_item.rs:[0-9]\+)$/,+3d" | # https://github.com/servo/servo/issues/46022
     grep -B5 -A10 'servoshell::panic_hook::panic_hook';
     if [ $? -eq 0 ]; then
         echo -n $file | sed "s/.txt//"
